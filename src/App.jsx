0da42028af4d5dc6edd9c2825b73b9250196bde6
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-04-18 17:27:31 +0100

    Shell of project built, add interactivity using react hooks next

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import "./style.css"
+
 function App() {
+
   return (
-    <div>
-      <h1>To Do App</h1>
-    </div>
+    <>
+      <form>
+        <div className="form-row">
+          <label className="form-label" htmlFor="item"> New Item </label>
+          <input className="form-input" type="text" id="item" />
+          <button className="btn">Add</button>
+        </div>
+      </form>
+      <h1 className="header">To Do List</h1>
+      <ul>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item uno
+          </label>
+          <button className="btn-delete">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item twono
+          </label>
+          <button className="btn-delete">Delete</button>
+        </li>
+
+      </ul>
+    </>
   )
 }
 
